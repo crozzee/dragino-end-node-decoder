2dd0a3da2c4a1847005e047a374e341d40138967
David Croswell <david@croswell.com.au>
2023-03-26 14:27:55 +1100

    Fix to work with HW v1.4 - Payload 8 bytes

--- LDDS75/LDDS75_Decoder_V1.1.4.js
+++ LDDS75/LDDS75_Decoder_V1.1.4.js
@@ -1,25 +1,31 @@
 function Decoder(bytes, port) {
   // Decode an uplink message from a buffer
   // (array) of bytes to an object of fields.
-  var value=(bytes[0]<<8 | bytes[1]) & 0x3FFF;
-  var batV=value/1000;//Battery,units:V
-   
-  value=bytes[2]<<8 | bytes[3];
-  var distance=(value);//distance,units:mm
+  var len = bytes.length;
+  var value = ( bytes[0]<<8 | bytes[1]) & 0x3FFF;
+  var batV = value / 1000;    //Battery,units:V
 
-  var i_flag = bytes[4]; 
-  
-  value=bytes[5]<<8 | bytes[6];
-  if(bytes[5] & 0x80)
-  {value |= 0xFFFF0000;}
-  var temp_DS18B20=(value/10).toFixed(2);//DS18B20,temperature  
-  
-  var s_flag = bytes[7];	
+  var distance = 0;
+  if (len==8)
+  {
+   value = bytes[2]<<8 | bytes[3];
+   distance = (value);//distance,units:mm
+   if (value<20)
+    distance = "Invalid Reading";
+
+   interrupt = bytes[4];
+   value = bytes[5]<<8 | bytes[6];
+   temperature = (value);
+  }
+  else
+   distance = "No Sensor";
+
+  var sensor = bytes[len-1];
   return {
-       Bat:batV,
-       Distance:distance,
-	   Interrupt_flag:i_flag,
-	   TempC_DS18B20:temp_DS18B20,
-	   Sensor_flag:s_flag,
+       battery:batV ,
+       distance:distance,
+       temperature:temperature,
+       interrupt_status:interrupt,
+       sensor:sensor
   };
 }
